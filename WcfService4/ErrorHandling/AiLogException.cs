17980247739f0841e470732882a457ccf7ca6331
Soubhagya Dash <Soubhagya.Dash@microsoft.com>
2014-12-12 00:00:20 -0800

    init

---
+++ WcfService4/ErrorHandling/AiLogException.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Description;
+using System.ServiceModel.Dispatcher;
+using System.Web;
+using Microsoft.ApplicationInsights;
+
+namespace WcfService4.ErrorHandling
+{
+    public class AiLogExceptionAttribute : Attribute, IErrorHandler, IServiceBehavior
+    {
+        public void AddBindingParameters(ServiceDescription serviceDescription,
+            System.ServiceModel.ServiceHostBase serviceHostBase,
+            System.Collections.ObjectModel.Collection<ServiceEndpoint> endpoints,
+            System.ServiceModel.Channels.BindingParameterCollection bindingParameters)
+        {
+        }
+
+        public void ApplyDispatchBehavior(ServiceDescription serviceDescription, 
+            System.ServiceModel.ServiceHostBase serviceHostBase)
+        {
+            foreach (ChannelDispatcher disp in serviceHostBase.ChannelDispatchers)
+            {
+                disp.ErrorHandlers.Add(this);
+            }
+        }
+
+        public void Validate(ServiceDescription serviceDescription, 
+            System.ServiceModel.ServiceHostBase serviceHostBase)
+        {
+        }
+
+        bool IErrorHandler.HandleError(Exception error)
+        {
+            var ai = new TelemetryClient();
+            ai.TrackException(error);
+            return false;
+        }
+
+        void IErrorHandler.ProvideFault(Exception error, 
+            System.ServiceModel.Channels.MessageVersion version, 
+            ref System.ServiceModel.Channels.Message fault)
+        {
+        }
+    }
+}
